58048f406c179b40a18f2940512edfbe7fd60606
Sachen Sampath <scsampath@ucsb.edu>
2022-04-06 16:52:16 -0700

    codecov

--- src/test/java/edu/ucsb/cs156/spring/hello/HelloControllerTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/HelloControllerTest.java
@@ -26,6 +26,7 @@ public class HelloControllerTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
+                
                 .andExpect(content().string(equalTo("Greetings from scsampath!")));
     }
 }
